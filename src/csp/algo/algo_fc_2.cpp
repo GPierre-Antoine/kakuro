794d31a4872a41be2d33ba5e645d72d02d76bc7b
pierreantoine <pierreantoine.guillaume+github@gmail.com>
2018-05-25 14:21:32 +0200

    proche d'une solution

--- src/csp/algo/algo_fc_2.cpp
+++ src/csp/algo/algo_fc_2.cpp
@@ -6,6 +6,8 @@
 #include "../../ostream.h"
 
 
+#define lf __FILE__ << ":" << std::setw(3) << __LINE__ << " # "
+
 /**
  *
  * @param f
@@ -13,12 +15,12 @@
  * @param timestamp
  * @param type
  */
-void restrict_automatic(csp_variable_ptr &f, std::vector<csp::record> &history, std::size_t timestamp)
+void restrict_automatic(csp_variable_ptr &f, record_vector &history, std::size_t timestamp)
 {
     history.emplace_back(csp::record(record_type::automatic, f, timestamp));
 }
 
-void restrict_manual(csp_variable_ptr &f, std::vector<csp::record> &history, std::size_t timestamp)
+void restrict_manual(csp_variable_ptr &f, record_vector &history, std::size_t timestamp)
 {
     f->restrict_first();
     history.emplace_back(csp::record(record_type::manual, f, timestamp));
@@ -29,20 +31,76 @@ void restrict_manual(csp_variable_ptr &f, std::vector<csp::record> &history, std
  * @param f
  * @param history
  */
-void rollback(csp_variable_ptr &f, std::vector<csp::record> &history)
+void rollback_different(csp_variable_ptr &f, record_vector &history)
 {
-    while (!history.empty() && (!history.back().is_same_variable(*f) && history.back().timestamp == f->get_id()))
+    if (!history.empty())
+        cout << lf << "next timestamp : " << history.back().timestamp << endl;
+
+    while (!history.empty() && history.back().timestamp != f->get_id())
     {
+        cout << lf << "timestamp de reference : " << f->get_id() << " timestamp a enlever : " << history.back().timestamp << endl;
         history.pop_back();
     }
 }
 
-void release_auto(std::vector<csp::record> &history)
+void rollback_different_variable(csp_variable_ptr &f, record_vector &history)
+{
+
+    while (!history.empty() && !history.back().is_same_variable(*f))
+    {
+        cout << lf
+             << "id comparée: " << history.back().get_variable()->get_id() << endl
+             << lf
+             << "timestamp (non-comparé ): " << history.back().timestamp << endl
+                ;
+        history.pop_back();
+    }
+}
+
+
+void release_auto(record_vector &history)
 {
     while (!history.empty() && !history.back().is_manual())
     {
         history.pop_back();
     }
+}
+
+
+bool resolve_error(const variable_vector &variables, variable_vector::iterator &it_variable, record_vector &history)
+{
+    //there is an error
+    //it_variable has just been valuated
+    //history -> timestamp maps to the IDs of the assignated variables
+    //it_variable belongs to variables
+    //it_variable is not in error state
+    //error is after it_variable
+    //but error is caused by it_variable assignation.
+
+    while (true)
+    {
+        cout << lf << "error treatment rolling back all that is not : " << **it_variable << endl;
+
+        rollback_different_variable(*it_variable, history);
+
+        restrict_manual(*it_variable, history, (*it_variable)->get_id());
+        cout << lf << "error treatment after restriction :  " << edit_domain(**it_variable) << endl;
+        if (!(*it_variable)->has_empty_domain())
+        {
+            cout <<endl<< lf << "domains after treatment" << edit_fn(it_variable, variables.end(), edit_domain) << endl;
+            return true;
+        }
+        if (it_variable == variables.begin())
+        {
+            return false;
+        }
+        std::advance(it_variable, -1);
+    }
+    //outputs :
+    //either error resolved -> true
+    //or
+    //variables.begin() is empty -> false
+
 }
 
 csp::algo_fc_2::algo_fc_2(bool stop_at_first_result) : algorithm(std::string("FC2"), stop_at_first_result)
@@ -55,26 +113,37 @@ std::vector<std::vector<size_t>> csp::algo_fc_2::run(variable_vector &variables,
                                                      heuristic heuristic) const
 {
     std::vector<std::vector<size_t>> solutions;
-    std::vector<csp::record> history;
+    record_vector                    history;
     history.reserve(1000);
-    auto it_variable = variables.begin();
 
-    auto counter = 0ul;
+    auto it_variable = variables.begin();
 
     while (true)
     {
         //state is clear at the begining
 
-        //make a timestamp
-        std::size_t change_index;
+        for (auto i = variables.begin(); i!=it_variable;std::advance(i,1)){
+            if (!(*i)->is_valuated())
+            {
+                cout << lf << edit_fn(variables.begin(),it_variable,edit_value_v) << endl;
+                throw std::runtime_error("Error in previous data");
+            }
+        }
 
+
+        //check error
+        bool met_error = false;
+
+
+        cout << endl << lf << edit_fn(variables.begin(), it_variable, edit_value_v) << endl;
 
         if (it_variable == variables.end())
         {
+
+            cout << lf << "found solution" << endl;
             //found solution
             record_solution(solutions, variables);
 
-            constexpr const std::size_t explore = 10;
 
             if (stop_at_first_result)
             {
@@ -82,100 +151,68 @@ std::vector<std::vector<size_t>> csp::algo_fc_2::run(variable_vector &variables,
             }
             //it variable now re-points toward a csp::csp_variable
             it_variable = std::prev(it_variable);
+            met_error   = true;
+            //treat current affectation as error, it will be rolled-back the same
 
-            change_index = (*it_variable)->get_id();
-            release_auto(history);
-
-            while (!history.empty() && !history.back().is_same_variable(**it_variable))
-            {
-                //rollback to previous
-                (*it_variable)->unvaluate();
-                it_variable = std::prev(it_variable);
-            }
-            change_index = (*it_variable)->get_id();
-            restrict_manual(*it_variable, history, change_index);
-
-            if (!(*it_variable)->has_empty_domain())
-            {
-                (*it_variable)->assign_first_element_as_value();
-            }
-            break;
         }
         else
         {
+            cout << lf << "edit domain pre-sort" << edit_fn(it_variable, variables.end(), edit_domain) << endl;
             //sort with heuristic
             std::sort(it_variable, variables.end(), heuristic);
+            cout << lf << "edit domain post-sort" << edit_fn(it_variable, variables.end(), edit_domain) << endl;
 
             //assign new value
             (*it_variable)->assign_first_element_as_value();
 
-            //timestamp = current variable id
-            change_index = (*it_variable)->get_id();
+            cout << lf << edit_value_v(**it_variable) << endl;
 
             //reverbate changes
             for (const auto &i:constraints)
             {
                 if (i->has_only_one_variable_unvaluated_left())
                 {
-                    auto variable = i->get_last_unvaluated_variable();
+                    auto variable                      = i->get_last_unvaluated_variable();
                     auto domain_size_before_constraint = variable->get_available_size();
                     i->run_fc();
                     auto domain_size_after_constraint = variable->get_available_size();
-                    auto stack = domain_size_before_constraint - domain_size_after_constraint;
+                    auto stack                        = domain_size_before_constraint - domain_size_after_constraint;
                     while (stack > 0)
                     {
                         stack -= 1;
                         //record all new mecanisme under current variable id
-                        restrict_automatic(variable, history, change_index);
+                        restrict_automatic(variable, history, (*it_variable)->get_id());
                     }
                 }
             }
         }
 
-        //check error
-        bool met_error = false;
         for (const auto &i:variables)
         {
             if (i->has_empty_domain())
             {
+                cout << lf << "met error : " << *i << endl;
                 met_error = true;
-                break;
             }
         }
+
         //clear error and return to start
         if (met_error)
         {
-            while (!history.empty() && history.back().timestamp == change_index && !history.back().is_manual())
+            if (!resolve_error(variables, it_variable, history))
             {
-                history.pop_back();
-            }
-            restrict_manual(*it_variable, history, change_index);
-
-            while ((*it_variable)->has_empty_domain() && it_variable != variables.begin())
-            {
-                it_variable = std::prev(it_variable);
-                rollback(*it_variable, history);
-                change_index = (*it_variable)->get_id();
-                restrict_manual(*it_variable, history, change_index);
-            }
-
-            //if first variable is empty, then exit
-            // - variables are sorted only when free
-            // - and break if first is sorted
-            // therefore variable.begin has empty domain means itvariable=begin
-
-            if ((**variables.begin()).has_empty_domain())
-            {
+                //if could not resolve error, exit
+                // <=> backtracked until first variable has empty domain
                 break;
             }
-
+            cout << lf << "edit domain post-error" << edit_fn(it_variable, variables.end(), edit_domain) << endl;
             continue;
         }
+
+
         //now clear from error, go to next variable
         it_variable = std::next(it_variable);
-
     }
-
     return solutions;
 }
 
--- src/forward.h
+++ src/forward.h
@@ -13,6 +13,7 @@ namespace csp
 {
     class csp_variable;
     class csp_constraint;
+    class record;
 }
 
 typedef std::shared_ptr<csp::csp_variable> csp_variable_ptr;
@@ -23,6 +24,9 @@ typedef std::vector<csp_variable_ptr> variable_vector;
 typedef std::shared_ptr<csp::csp_constraint> csp_constraint_ptr;
 csp_constraint_ptr make_yield_c_sum(variable_vector &, std::size_t);
 csp_constraint_ptr make_yield_c_diff(variable_vector &);
+
+
+typedef std::vector<csp::record> record_vector;
 
 template<typename iter_t, class T, class type>
 T reduce(iter_t begin, iter_t end, T init, std::function<T (T,type)> f)
