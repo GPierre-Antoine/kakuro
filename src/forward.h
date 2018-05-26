1503b30e7b4f8b000ce088e7a24c8bfb647dba0c
Pierre-Antoine GUILLAUME <pierreantoine.guillaume+github@gmail.com>
2018-05-26 22:47:44 +0200

    omission in prev commit

--- src/forward.h
+++ src/forward.h
@@ -22,11 +22,10 @@ csp_variable_ptr make_yield_variable(std::size_t);
 typedef std::vector<csp_variable_ptr> variable_vector;
 
 typedef std::shared_ptr<csp::csp_constraint> csp_constraint_ptr;
-csp_constraint_ptr make_yield_c_sum(variable_vector &, std::size_t);
-csp_constraint_ptr make_yield_c_diff(variable_vector &);
+csp_constraint_ptr make_yield_c_sum(std::size_t);
+csp_constraint_ptr make_yield_c_diff();
 
-
-typedef std::vector<csp::record> record_vector;
+typedef std::stack<csp::record> record_vector;
 
 template<typename iter_t, class T, class type>
 T reduce(iter_t begin, iter_t end, T init, std::function<T (T,type)> f)
